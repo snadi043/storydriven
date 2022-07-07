a414224fb76854db678d1eea1e3917828a1f8fe0
Santhosh Elumalai <Santhosh.Elumalai@ddiworld.com>
2022-07-07 17:56:06 +0530

    Added new changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
+@HostListener('window:scroll', ['$event'])
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    window.addEventListener('scroll',this.onWindowScroll, true)
   }
 
+  onWindowScroll() {
+    let element = document.querySelector('.navbar') as HTMLElement;
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('grey-nav');
+    } else {
+      element.classList.remove('grey-nav');
+    }
+  }
 }
